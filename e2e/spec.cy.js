d6a35ae24288d5481bb0d92d5b3302241a922277
tadas <tdavidsonas88@gmail.com>
2022-12-13 16:41:20 +0200

    first commit

---
+++ e2e/spec.cy.js
@@ -0,0 +1,41 @@
+describe('My First Test', () => {
+  it('Checks if BTCUSD 5 days performance rate change was positive', () => {
+
+    const getIframeDocument = (iframeName) => {
+      return cy
+      .get('iframe[id="' + iframeName +'"]')
+      // Cypress yields jQuery element, which has the real
+      // DOM element under property "0".
+      // From the real DOM iframe element we can get
+      // the "document" element, it is stored in "contentDocument" property
+      // Cypress "its" command can access deep properties using dot notation
+      // https://on.cypress.io/its
+      .its('0.contentDocument').should('exist')
+    }
+    
+    const getIframeBody = (iframeName) => {
+      // get the document
+      return getIframeDocument(iframeName)
+      // automatically retries until body is loaded
+      .its('body').should('not.be.undefined')
+      // wraps "body" DOM element to allow
+      // chaining more Cypress commands, like ".find(...)"
+      .then(cy.wrap)
+    }
+    
+    cy.visit('https://www.marketwatch.com/')
+
+    getIframeBody('sp_message_iframe_719544').find('.sp_choice_type_11').click()
+
+    cy.get('button.j-btn-search').click()
+    
+    cy.get('input#mw-search').type('BTCUSD', {delay: 0})
+
+    cy.wait(7000)
+
+    cy.get('a.j-result-link').first().click()
+
+    cy.get('table.c2 .value').first().invoke('text').then(parseFloat).should('be.gt', 0)
+
+  })
+})
